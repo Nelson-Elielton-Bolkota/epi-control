4dbbfe4b22215e158a69711a1c242f6a8859b528
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-09-09 23:09:53 -0300

    rota FuncionarioController

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EpiController;
+use App\Http\Controllers\FuncionarioController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('epis', EpiController::class);
+Route::resourse('funcionarios',FuncionarioController::class);
