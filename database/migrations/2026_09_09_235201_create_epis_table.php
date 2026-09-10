694a6b99d2a3e1a4d246d646c5f720f64f6ad1b0
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-09-09 21:34:08 -0300

    definição do databaseSeeder para chamar EpiSeeder

--- database/migrations/2026_09_09_235201_create_epis_table.php
+++ database/migrations/2026_09_09_235201_create_epis_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
          Schema::create('epis', function (Blueprint $table) {
             $table->id();
             $table->string('nome', 100);
+            $table->string('tipo',100);
             $table->string('ca', 6);
             $table->string('fabricante', 100);
             $table->integer('quantidade');
