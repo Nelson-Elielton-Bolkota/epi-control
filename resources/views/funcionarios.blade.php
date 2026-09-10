05ec2e374ee1923e5593e2fa5518af854332816e
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-09-09 23:31:35 -0300

    alteração na view funcionario

--- resources/views/funcionarios.blade.php
+++ resources/views/funcionarios.blade.php
@@ -1,21 +1,22 @@
 @extends('layouts.app')
 
-@section('title', 'EPIs')
+@section('title', 'Funcionários')
 
 @section('content')
 
-    <h2>Lista de Funcionarios</h2>
+    <h2>Lista de Funcionários</h2>
 
-	@foreach ( $funcionarios as $funcionario)
-		<p>Nome: {{ $funcionario->nome }}</p>
+    @foreach ($funcionarios as $funcionario)
+
+        <p>Nome: {{ $funcionario->nome }}</p>
         <p>CPF: {{ $funcionario->cpf }}</p>
-        <p>Cargo: {{ $funcionario->cargo}}</p> 
-		<p>Setor: {{ $funcionario->setor}}</p> 
-		<p>Data_Adm {{ $funcionario->data_admissao}}</p>
-		<p>Status: {{ $funcionario->status}}</p> 
-		
-	</a>
+        <p>Cargo: {{ $funcionario->cargo }}</p>
+        <p>Setor: {{ $funcionario->setor }}</p>
+        <p>Data de admissão: {{ $funcionario->data_admissao }}</p>
+        <p>Status: {{ $funcionario->status }}</p>
 
-	<hr>
+        <hr>
+
+    @endforeach
 
 @endsection
