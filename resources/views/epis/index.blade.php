5eae983651d07ded3b1b6957b28eb6c472621752
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-09-09 22:20:53 -0300

    criação do epis/index

--- resources/views/epis/index.blade.php
+++ resources/views/epis/index.blade.php
@@ -3,5 +3,20 @@
 @section('title', 'EPIs')
 
 @section('content')
-<p>Ola cu</p>
+
+    <h2>Lista de EPIs</h2>
+
+    <a href="{{ route('epis.create') }}"> Adicionar novo EPI</a>
+	@foreach ( $epis as $epi)
+		<p>Nome: {{ $epi->nome }}</p>
+        <p>Tipo: {{ $epi->tipo }}</p>
+        <p>Ca: {{ $epi->ca}}</p>  
+		<p>Ca: {{ $epi->ca}}</p> 
+		<p>Fabricante: {{ $epi->fabricante}}</p> 
+		<p>Quantidade: {{ $epi->quantidade}}</p> 
+		<p>Validade: {{ $epi->validade}}</p> 
+		<p>Status: {{ $epi->status}}</p>  
+        <hr>
+	@endforeach
+
 @endsection
