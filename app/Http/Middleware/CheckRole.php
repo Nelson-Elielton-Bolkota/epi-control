46957e0ac27be4986e06f8cb4e43a1248272ecf3
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-09-11 19:08:54 -0300

    definição das regras de middleware

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -13,8 +13,12 @@ class CheckRole
      *
      * @param  Closure(Request): (Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
+        if (!in_array($request->user()->role, $roles)) {
+            abort(403);
+        }
+
         return $next($request);
     }
 }
